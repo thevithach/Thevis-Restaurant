9dc398181ddba83c42aea93bee0e01bc30c6781c
thevithach <vitt@uia.no>
2024-03-03 15:30:15 +0100

    Fixing Header and footer UI

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer fixed-bottom text-center p-3 bg-dark text-white">
+      Lagd av The Vi
+      <i className="p-2 bi bi-github"></i>
+    </div>
+  );
+}
+
+export default Footer;
