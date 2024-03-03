10031b015ed5c9c8213fe594d8f2c1bb9193a989
thevithach <vitt@uia.no>
2024-03-03 15:41:30 +0100

    adding interface for menuitems and fetching all of them.

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
 import { Footer, Header } from "../Components/Layout";
+import { useEffect, useState } from "react";
+import { menuItemModel } from "../Interfaces";
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch("https://thevireact.azurewebsites.net/api/MenuItem")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
