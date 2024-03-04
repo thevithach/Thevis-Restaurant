b53edbe3fc5c3915e6a12e7dea7805df982521c7
thevithach <vitt@uia.no>
2024-03-04 14:49:51 +0100

    Adding home, list and menucard components

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,23 +1,11 @@
 import { Footer, Header } from "../Components/Layout";
-import { useEffect, useState } from "react";
-import { menuItemModel } from "../Interfaces";
+import { Home } from "../Pages";
 
 function App() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
-
-  useEffect(() => {
-    fetch("https://thevireact.azurewebsites.net/api/MenuItem")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result);
-      });
-  }, []);
-
   return (
     <div>
       <Header />
-      Main Component
+      <Home />
       <Footer />
     </div>
   );
