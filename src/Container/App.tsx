8679f8fea9b0c66d6c18d281c09bee72518cd3e3
thevithach <vitt@uia.no>
2024-03-06 17:24:54 +0100

    Added items to shoppingcartQuery, but its connected to a hardcoded user. Will fix later.

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,8 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import { Footer, Header } from "../Components/Layout";
 import { Home, MenuItemDetails, NotFound } from "../Pages";
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
+import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
 function App() {
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useGetShoppingCartQuery(
+    "709d1276-6928-4a87-b8e6-8426bb1ebadf"
+  );
+
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result.cartItems));
+    }
+  }, [data]);
+
   return (
     <div>
       <Header />
