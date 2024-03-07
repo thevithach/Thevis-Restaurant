77c50648de14d77992623d3403b782643b265aac
thevithach <vitt@uia.no>
2024-03-07 22:59:37 +0100

    Added controller components into register

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { SD_Roles } from "../Utility/SD";
+import { inputHelper } from "../Helper";
 
 function Register() {
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    name: "",
+    password: "",
+    role: "",
+  });
+
+  const handleUserInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempData = inputHelper(e, userInput);
+    setUserInput(tempData);
+  };
+
   return (
     <div className="container text-center">
       <form method="post">
@@ -12,6 +29,9 @@ function Register() {
               className="form-control"
               placeholder="Enter Username"
               required
+              name="userName"
+              value={userInput.userName}
+              onChange={handleUserInput}
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -20,6 +40,9 @@ function Register() {
               className="form-control"
               placeholder="Enter Name"
               required
+              name="name"
+              value={userInput.name}
+              onChange={handleUserInput}
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -28,13 +51,22 @@ function Register() {
               className="form-control"
               placeholder="Enter Password"
               required
+              name="password"
+              value={userInput.password}
+              onChange={handleUserInput}
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-            <select className="form-control form-select" required>
-              <option value="">--Select Role--</option>
-              <option value="customer">Customer</option>
-              <option value="admin">Admin</option>
+            <select
+              className="form-control form-select"
+              required
+              name="role"
+              value={userInput.role}
+              onChange={handleUserInput}
+            >
+              <option value="">-- Select Role --</option>
+              <option value={`${SD_Roles.CUSTOMER}`}>Customer</option>
+              <option value={`${SD_Roles.ADMIN}`}>Admin</option>
             </select>
           </div>
         </div>
