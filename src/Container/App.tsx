c0e760571676d51bf9938e2ed63f0f8358c237b4
thevithach <vitt@uia.no>
2024-03-09 23:04:33 +0100

    Fixing authentication bug, stored in local storage, but not logged in on browser.

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -12,12 +12,24 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
 import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
+import { userModel } from "../Interfaces";
+import jwt_decode from "jwt-decode";
+import { setLoggedInUser } from "../Storage/Redux/userAuthSlice";
 function App() {
   const dispatch = useDispatch();
 
   const { data, isLoading } = useGetShoppingCartQuery(
     "709d1276-6928-4a87-b8e6-8426bb1ebadf"
   );
+
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+
+    if (localToken) {
+      const { fullName, id, email, role }: userModel = jwt_decode(localToken);
+      dispatch(setLoggedInUser({ fullName, id, email, role }));
+    }
+  }, []);
 
   useEffect(() => {
     if (!isLoading) {
