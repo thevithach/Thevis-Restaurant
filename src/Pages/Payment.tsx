b97cf29f4e9f8446577b9b2a744a08b2e88baf1c
thevithach <vitt@uia.no>
2024-03-13 17:19:55 +0100

    Adding the stripe payment as per stripe documentation

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { useLocation } from "react-router";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
+  const stripePromise = loadStripe(
+    "pk_test_51O9wE0I1b2IdnTWddr1omvh5vLpLDHyHiaH9dZoBerSAccEIjnMe0AnUytsQwt2zxJJoqSrkEpj88a6KcwFu3IWv007qQwlknJ"
+  );
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+
   console.log(apiResult);
   console.log(userInput);
 
-  return <div>Payment</div>;
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
