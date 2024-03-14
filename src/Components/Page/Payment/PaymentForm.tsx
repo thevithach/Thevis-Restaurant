0bc94cf47213c1613094562bd027ea4b964ce261
thevithach <vitt@uia.no>
2024-03-14 17:11:30 +0100

    Removing the redirect to example domain..

--- src/Components/Page/Payment/PaymentForm.tsx
+++ src/Components/Page/Payment/PaymentForm.tsx
@@ -1,8 +1,46 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import { set } from "immer/dist/internal";
+import { useState } from "react";
+import { toastNotify } from "../../../Helper";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isProcessing, setProcessingTo] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    setProcessingTo(true);
+
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+      redirect: "if_required",
+    });
+
+    if (result.error) {
+      // Show error to your customer (for example, payment details incomplete)
+      toastNotify("An unexpected error occurred", "error");
+      setProcessingTo(false);
+    } else {
+      console.log(result);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
       <button className="btn btn-success mt-5 w-100">Submit</button>
     </form>
