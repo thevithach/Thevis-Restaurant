643244adac5653d44ebfedc7a4af20e442ed9a5c
thevithach <vitt@uia.no>
2024-03-16 18:07:18 +0100

    Adding the update method for orders so that i can change status.

--- src/Apis/orderApi.ts
+++ src/Apis/orderApi.ts
@@ -33,6 +33,17 @@ const orderApi = createApi({
       }),
       providesTags: ["Orders"],
     }),
+    updateOrderHeader: builder.mutation({
+      query: (orderDetails) => ({
+        url: "order" + orderDetails.orderHeaderId,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: orderDetails,
+      }),
+      invalidatesTags: ["Orders"],
+    }),
   }),
 });
 
@@ -40,5 +51,6 @@ export const {
   useCreateOrderMutation,
   useGetAllOrdersQuery,
   useGetOrderDetailsQuery,
+  useUpdateOrderHeaderMutation,
 } = orderApi;
 export default orderApi;
